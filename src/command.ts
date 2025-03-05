f69b9c8f5f0fdc2510ead84ee10227b1f138f258
bankjirapan <jirapan_yankhan@hotmail.com>
2025-03-05 18:07:03 +0700

    reflector: format code

--- src/command.ts
+++ src/command.ts
@@ -2,25 +2,25 @@
  * Enum representing commands used in Longdo Spell extension.
  */
 export enum Command {
-    /**
-     * Command to check spelling in the current document.
-     */
-    CheckSpelling = 'longdo-spell.spell',
+  /**
+   * Command to check spelling in the current document.
+   */
+  CheckSpelling = "longdo-spell.spell",
 
-    /**
-     * Command to clear spelling decorations in the current document.
-     */
-    ClearSpell = 'longdo-spell.clear',
+  /**
+   * Command to clear spelling decorations in the current document.
+   */
+  ClearSpell = "longdo-spell.clear",
 
-    /**
-     * Command to open the set key.
-     */
-    SetAPIKey = 'longdo-spell.setKey',
+  /**
+   * Command to open the set key.
+   */
+  SetAPIKey = "longdo-spell.setKey",
 
-    /**
-     * 
-     * Command to open the settings UI.
-     * 
-     */
-    openSettingUI = 'longdo-spell.openSettings'
+  /**
+   *
+   * Command to open the settings UI.
+   *
+   */
+  openSettingUI = "longdo-spell.openSettings",
 }
