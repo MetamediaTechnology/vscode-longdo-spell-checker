5be1a4b85a89cfbf706ea7efef4beb09b4815382
bankjirapan <jirapan_yankhan@hotmail.com>
2025-03-13 12:19:11 +0700

    feat: auto hide bottom bar

--- src/ui.ts
+++ src/ui.ts
@@ -1,12 +1,17 @@
 import * as vscode from 'vscode';
 import { Command } from './command';
 
+const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+statusBar.text = "$(pass) Longdo Spell";
+statusBar.command = Command.showQuickPick;
 
-export function statusBar(context: vscode.ExtensionContext) {
-  const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-  statusBar.text = "$(pass) Longdo Spell";
-  statusBar.command = Command.showQuickPick;
+export function showStatusBar(context: vscode.ExtensionContext) {
+
   statusBar.show();
   context.subscriptions.push(statusBar);
+}
 
+export function hideStatusBar(context: vscode.ExtensionContext) {
+  statusBar.hide();
+  context.subscriptions.push(statusBar);
 }
