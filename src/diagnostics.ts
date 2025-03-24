4b6a2a9f3b6ec6e52c80495dfbc70360f170a399
bankjirapan <jirapan_yankhan@hotmail.com>
2025-03-24 18:14:48 +0700

    reflector: use tranlsate file value

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,41 +1,46 @@
 import * as vscode from "vscode";
 import { ErrorsResult } from "./interface/types";
+import { Language } from "./language";
 
-let diagnosticCollection: vscode.DiagnosticCollection = vscode.languages.createDiagnosticCollection("longdoSpell");
+const lang = Language.getInstance();
+let diagnosticCollection: vscode.DiagnosticCollection =
+  vscode.languages.createDiagnosticCollection("longdoSpell");
 
-export function onClearDiagnostics() {
-  if (diagnosticCollection) {
+export class Diagnostics {
+  /**
+   * Clears all diagnostics from the editor
+   */
+  public static clearDiagnostics() {
     diagnosticCollection.clear();
   }
-}
 
-export function onShowDiagnostics(
-  results: ErrorsResult[],
-  editor: vscode.TextEditor,
-) {
-    
-  diagnosticCollection.clear();
+  /**
+   * Shows diagnostics in the editor
+   * @param results - the errors result
+   * @param editor - the text editor
+   */
+  public static onShowDiagnostics(results: ErrorsResult[], editor: vscode.TextEditor) {
+    diagnosticCollection.clear();
 
-  const diagnostics: vscode.Diagnostic[] = results.map((error) => {
-    const { line, start } = error.originalPosition;
-    const wordLength = error.word?.length || 0;
-    const range = new vscode.Range(
-      new vscode.Position(line, start),
-      new vscode.Position(line, start + wordLength)
-    );
+    const diagnostics: vscode.Diagnostic[] = results.map((error) => {
+      const { line, start } = error.originalPosition;
+      const wordLength = error.word?.length || 0;
+      const range = new vscode.Range(
+        new vscode.Position(line, start),
+        new vscode.Position(line, start + wordLength)
+      );
 
-    const diagnostic = new vscode.Diagnostic(
-      range,
-      `คำที่อาจสะกดผิด: "${error.word}"${
-        error.suggests.length
-          ? `. แนะนำ: ${error.suggests.join(", ")}`
-          : ""
-      }`,
-      vscode.DiagnosticSeverity.Information
-    );
+      const diagnostic = new vscode.Diagnostic(
+        range,
+        `${lang.getTranslation("potentialMistake")} "${error.word}"${
+          error.suggests.length ? `. ${lang.getTranslation("suggest")}: ${error.suggests.join(", ")}` : ""
+        }`,
+        vscode.DiagnosticSeverity.Information
+      );
 
-    diagnostic.source = "Longdo Spell Checker";
-    return diagnostic;
-  });
-  diagnosticCollection.set(editor.document.uri, diagnostics);
+      diagnostic.source = "Longdo Spell Checker";
+      return diagnostic;
+    });
+    diagnosticCollection.set(editor.document.uri, diagnostics);
+  }
 }
