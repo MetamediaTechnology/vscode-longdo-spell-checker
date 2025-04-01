cb74d9176384a11e5536cb857bd5fa0239071d6b
bankjirapan <jirapan_yankhan@hotmail.com>
2025-04-01 19:50:44 +0700

    feat: Chack internet connection when offlone wil not show notification

--- src/api.ts
+++ src/api.ts
@@ -1,29 +1,57 @@
 import { ApiResponse } from "./interface/types";
 import * as vscode from "vscode";
 
-
-async function postProof(text: string) {
-    try {
-      const response = await fetch("https://api.longdo.com/spell-checker/proof", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          text,
-          key: vscode.workspace.getConfiguration("longdo-spell").get("apiKey"),
-        }),
-      });
-
-      const result = await response.json() as ApiResponse;
-      if (!result.status || result.status !== 200) {
-        throw new Error(result?.message || "API Error");
-      }
-      return result;
-    } catch (error: unknown) {
-      throw error;
-    }
+/**
+ * 
+ * @returns 
+ * Check if the internet connection is available
+ * @throws Error if the network is not available
+ */
+async function checkInternetConnection(): Promise<boolean> {
+  try {
+    await fetch('https://api.longdo.com', { 
+      method: 'HEAD',
+      mode: 'no-cors',
+    });
+    return true;
+  } catch (error) {
+    return false;
   }
+}
+
+/**
+ * 
+ * @param text 
+ * @returns json data from Longdo API
+ * @throws Error if the API request fails
+ */
+async function postProof(text: string) {
+  try {
+    const isConnected = await checkInternetConnection();
+    if (!isConnected) {
+      throw new Error("NetworkError");
+    }
+    
+    const response = await fetch("https://api.longdo.com/spell-checker/proof", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        key: vscode.workspace.getConfiguration("longdo-spell").get("apiKey"),
+      }),
+    });
+
+    const result = await response.json() as ApiResponse;
+    if (!result.status || result.status !== 200) {
+      throw new Error(result?.message || "API Error");
+    }
+    return result;
+  } catch (error: unknown) {
+    throw error;
+  }
+}
 
 export {
     postProof
