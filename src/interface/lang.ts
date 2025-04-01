8f5e8e6ce35d0661374b6e29fc3172bbab599dbf
bankjirapan <jirapan_yankhan@hotmail.com>
2025-04-01 19:52:50 +0700

    feat: add shiki libs for code hightlight

--- src/interface/lang.ts
+++ src/interface/lang.ts
@@ -1,18 +1,20 @@
-const languageMap: Record<string, string> = {
-    "js": "javascript",
-    "ts": "typescript",
-    "jsx": "javascript",
-    "tsx": "typescript",
-    "html": "xml",
-    "css": "css",
-    "py": "python",
-    "php": "php",
-    "rb": "ruby",
-    "java": "java",
-    "json": "json",
-    "md": "markdown",
-    "vue": "javascript"
-  };
+import type { BundledLanguage } from "shiki" with { "resolution-mode": "import" };
+const languageMap: Record<string, BundledLanguage> = {
+  "js": "javascript",
+  "ts": "typescript",
+  "jsx": "javascript",
+  "tsx": "typescript",
+  "html": "xml",
+  "css": "css",
+  "py": "python",
+  "php": "php",
+  "rb": "ruby",
+  "java": "java",
+  "json": "json",
+  "md": "markdown",
+  "vue": "vue",
+  "dart": "dart",
+};
 
 export {
     languageMap
