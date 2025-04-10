6c37e7f97020590d6163ff5ce2a2411c93fedb84
bankjirapan <jirapan_yankhan@hotmail.com>
2025-04-10 11:38:02 +0700

    feat: add typescript support

--- src/interface/lang.ts
+++ src/interface/lang.ts
@@ -1,6 +1,7 @@
 import type { BundledLanguage } from "shiki" with { "resolution-mode": "import" };
 const languageMap: Record<string, [BundledLanguage, string[]]> = {
   "js": ["javascript", ["#A3BE8C"]],
+  "ts": ["typescript", ["#A3BE8C"]],
   "json": ["json", ["#A3BE8C"]],
   "md": ["markdown", ["#D8DEE9FF"]],
 };
